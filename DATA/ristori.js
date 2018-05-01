3ae251d43ab407e36365886f8a3539a6de7e1d3e
LucaMoiana <luca_moiana@hotmail.com>
2018-05-01 19:10:14 +0200

    upload
    upload

---
+++ DATA/ristori.js
@@ -0,0 +1,10 @@
+var ristori = [{
+"type": "FeatureCollection",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "nome": "ristoro smaria", "X": 45.651081575394848, "Y": 8.9683991558110243 }, "geometry": { "type": "Point", "coordinates": [ 8.968399155811024, 45.651081575394848 ] } },
+{ "type": "Feature", "properties": { "nome": "ristoro massina", "X": 45.6398993999927, "Y": 8.9654026 }, "geometry": { "type": "Point", "coordinates": [ 8.967807761199575, 45.63988215475915 ] } },
+{ "type": "Feature", "properties": { "nome": "ristoro roccolo", "X": 45.641661216226915, "Y": 8.9536514985312969 }, "geometry": { "type": "Point", "coordinates": [ 8.953651498531297, 45.641661216226915 ] } },
+{ "type": "Feature", "properties": { "nome": "ristoro visconta", "X": 45.651872831999938, "Y": 8.9471388579782083 }, "geometry": { "type": "Point", "coordinates": [ 8.947138857978208, 45.651872831999938 ] } }
+]
+}]
